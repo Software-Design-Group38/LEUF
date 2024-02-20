5fa985fa77aa48076affca5175e2c553dc7ceb84
MichaelN <mn04202002@gmail.com>
2024-02-19 22:15:13 -0600

    Created the login page
    has username and password field and also login and create account buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,9 @@ import React, { lazy } from 'react'
 import Navbar from './Components/Navbar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-const Home = lazy(() => import('./Pages/Home'));
+const Home = lazy(() => import('./Pages/home/Home'));
 const FuelForm = lazy(() => import('./Pages/fuelForm/FuelForm'));
+const CreateAccount = lazy(() => import('./Pages/createAcc/CreateAcc'));
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/fuelform" element={<FuelForm />}/>
+        <Route path="/createacc" element={<CreateAccount />}/>
       </Routes>
   </Router>
   );
