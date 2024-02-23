c57b08c4bd2f3c49080722c9f7b6454cf7069e86
John Nguyen <johnnguyen1820@gmail.com>
2024-02-23 17:08:32 -0600

    styling updates

--- frontend/src/Pages/fuelForm/FuelForm.jsx
+++ frontend/src/Pages/fuelForm/FuelForm.jsx
@@ -34,58 +34,70 @@ const FuelForm = () => {
   };
 
   return (
-    <div className='h-screen bg-gradient-to-b from-[#C5CCCE] to-[#2B475F]'>
-      <h2>Fuel Quote Form</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="gallonsRequested">Gallons Requested:</label>
-          <input
-            type="number"
-            id="gallonsRequested"
-            value={gallonsRequested}
-            onChange={(e) => setGallonsRequested(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="deliveryAddress">Delivery Address:</label>
-          <input
-            type="text"
-            id="deliveryAddress"
-            value="123 Main St, City, State" // Example client profile address
-            readOnly
-          />
-        </div>
-        <div>
-          <label htmlFor="deliveryDate">Delivery Date:</label>
-          <input
-            type="date"
-            id="deliveryDate"
-            value={deliveryDate}
-            onChange={(e) => setDeliveryDate(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="suggestedPrice">Suggested Price / gallon:</label>
-          <input
-            type="number"
-            id="suggestedPrice"
-            value="2.50" // Example suggested price per gallon
-            readOnly
-          />
-        </div>
-        <div>
-          <label htmlFor="totalAmountDue">Total Amount Due:</label>
-          <input
-            type="number"
-            id="totalAmountDue"
-            value={(parseFloat(gallonsRequested) * 2.50).toFixed(2)} // Example calculation
-            readOnly
-          />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+    <div className='flex justify-center items-center flex-col h-screen bg-gradient-to-b from-[#C5CCCE] to-[#2B475F]'>
+      <div className='bg-white rounded-md p-6 w-122'>
+        <h2 className='mb-4'>Fuel Quote Form</h2>
+        <form onSubmit={handleSubmit}>
+          <div className=''>
+            <label htmlFor="gallonsRequested" className='w-1/2 font-semibold text-lg pr-1.5'>Gallons Requested:</label>
+            <input
+              className='w-1/2 border rounded'
+              placeholder='0'
+              min={0}
+              type="number"
+              id="gallonsRequested"
+              value={gallonsRequested}
+              onChange={(e) => setGallonsRequested(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="deliveryAddress" className='w-1/2 font-semibold text-lg pr-1.5'>Delivery Address:</label>
+            <input
+              className='w-1/2 border rounded'
+              type="text"
+              id="deliveryAddress"
+              value="123 Main St, City, State" // Example client profile address
+              readOnly
+            />
+          </div>
+          <div>
+            <label htmlFor="deliveryDate" className='w-1/2 font-semibold text-lg pr-1.5'>Delivery Date:</label>
+            <input
+              className='w-1/2 border rounded'
+              type="date"
+              id="deliveryDate"
+              value={deliveryDate}
+              onChange={(e) => setDeliveryDate(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="suggestedPrice" className='w-1/2 font-semibold text-lg pr-1.5'>Suggested Price / Gallon:</label>
+            <span>$</span>
+            <input
+              className='w-40 border rounded'
+              type="number"
+              id="suggestedPrice"
+              value="2.50" // Example suggested price per gallon
+              readOnly
+            />
+          </div>
+          <div>
+            <label htmlFor="totalAmountDue" className='w-1/2 font-semibold text-lg pr-1.5'>Total Amount Due:</label>
+            <span>$</span>
+            <input
+              className='w-1/2 border rounded'
+              type="number"
+              placeholder='0'
+              id="totalAmountDue"
+              value={(parseFloat(gallonsRequested) * 2.50).toFixed(2)} // Example calculation
+              readOnly
+            />
+          </div>
+          <button type="submit" className='w-full mt-4 hover:bg-gray-500 hover:text-white shadow appearance-none border rounded'>Submit</button>
+        </form>
+      </div>
     </div>
   );
 };
--- frontend/src/Pages/history/History.jsx
+++ frontend/src/Pages/history/History.jsx
@@ -3,20 +3,77 @@ import { Link } from 'react-router-dom'
 
 const History = () => {
   const [fuelQuotes, setFuelQuotes] = useState([]);
+
   return (
     <div className='h-screen bg-gradient-to-b from-[#C5CCCE] to-[#2B475F]'>
-      <h2>Fuel Quote History</h2>
-      <ul>
-        {fuelQuotes.map((quote, index) => (
-          <li key={index}>
-            {/*Display quote details*/}
-            Gallons Requested: {quote.gallonsRequested}, Delivery Date: {quote.deliveryDate}, Total Amount Due: {quote.totalAmountDue.toFixed(2)}
-          </li>
-        ))}
-      </ul>
-      {/*Should this be a link back to fuel form or home or two links? */}
-      <Link to="/fuelform">Back to Fuel Form</Link>
-      <Link to="/home">Back to Home</Link>
+      <div className="flex flex-col overflow-y-auto justify-center items-center">
+        <h2 className='m-10'>Fuel Quote History</h2>
+        <div className="sm:-mx-6 lg:-mx-8">
+          <div className="inline-block py-2 sm:px-6 lg:px-8">
+            <div className="overflow-y-auto">
+              <table className="text-left text-sm font-light">
+                <thead className="border-b font-medium dark:border-neutral-500">
+                  <tr>
+                    <th scope="col" className="px-6 py-4">Gallons Requested</th>
+                    <th scope="col" className="px-6 py-4">Delivery Address</th>
+                    <th scope="col" className="px-6 py-4">Delivery Date</th>
+                    <th scope="col" className="px-6 py-4">Suggested Price / gallon</th>
+                    <th scope="col" className="px-6 py-4">Total Amount</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr className="border-b dark:border-neutral-500 hover:bg-neutral-100">
+                    <td className="whitespace-nowrap px-6 py-4">N/A</td>
+                    <td className="whitespace-nowrap px-6 py-4">N/A</td>
+                    <td className="whitespace-nowrap px-6 py-4">N/A</td>
+                    <td className="whitespace-nowrap px-6 py-4">N/A</td>
+                    <td className="whitespace-nowrap px-6 py-4">N/A</td>
+                  </tr>
+                  <tr className="border-b dark:border-neutral-500 hover:bg-neutral-100">
+                    <td className="whitespace-nowrap px-6 py-4">N/A</td>
+                    <td className="whitespace-nowrap px-6 py-4">N/A</td>
+                    <td className="whitespace-nowrap px-6 py-4">N/A</td>
+                    <td className="whitespace-nowrap px-6 py-4">N/A</td>
+                    <td className="whitespace-nowrap px-6 py-4">N/A</td>
+                  </tr>
+                  <tr className="border-b dark:border-neutral-500 hover:bg-neutral-100">
+                    <td className="whitespace-nowrap px-6 py-4">N/A</td>
+                    <td className="whitespace-nowrap px-6 py-4">N/A</td>
+                    <td className="whitespace-nowrap px-6 py-4">N/A</td>
+                    <td className="whitespace-nowrap px-6 py-4">N/A</td>
+                    <td className="whitespace-nowrap px-6 py-4">N/A</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+        <ul>
+          <li><a href='#fuelform' className='hover:text-white' onClick={()=>{window.location.replace('/fuelform')}}>Back to Fuel Quote Form</a></li>
+        </ul>
+      </div>
+     {/* <div className='flex flex-col justify-center items-center'>
+        <h2 className='m-10'>Fuel Quote History</h2>
+        <div className='h-200 border-2 rounded-md'>
+          <ul className='flex flex-row'>
+            <li>Gallons Requested</li>
+            <li>Delivery Address</li>
+            <li>Delivery Date</li>
+            <li>Suggested Price / Gallon</li>
+            <li>Total Amount Due</li>
+            {fuelQuotes.map((quote, index) => (
+              <li key={index}>
+                {/*Display quote details
+                Gallons Requested: {quote.gallonsRequested}, Delivery Date: {quote.deliveryDate}, Total Amount Due: {quote.totalAmountDue.toFixed(2)}
+              </li>
+            ))}
+          </ul>
+        </div>
+        {/*Should this be a link back to fuel form or home or two links?
+        <Link to="/fuelform">Back to Fuel Form</Link>
+        <li><a href='#fuelform' onClick={()=>{window.location.replace('/fuelform')}}>Back to Fuel Quote Form</a></li>
+        {/*<Link to="/home">Back to Home</Link>
+      </div> */}
     </div>
   );
 };
