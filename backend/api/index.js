12c92e62b926dcca5593e253545e30fba0da23af
John Nguyen <johnnguyen1820@gmail.com>
2024-04-08 20:33:41 -0500

    database setup
    unable to connect to db

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,14 +1,27 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const port = 3001
+const dotenv = require('dotenv')
 const routers = require("../routes.js")
+const db = require("../database.js")
+dotenv.config()
 
 app.use(express.json())
 app.use(cors())
+const port = 3001 || process.env.PORT
 
 app.get('/', (req, res) => {
   res.send("Hello")
+})
+
+db.connect((err) => {
+  if (err){
+    console.error("Database connection failed: " + err.stack)
+    return
+  }
+  else{
+    console.log("Connected to database")
+  }
 })
 
 app.use(routers)
